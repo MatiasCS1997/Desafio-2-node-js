7ce63ceb085afa94772231cd8b9de3a74dd0de15
ycstuardo <104527883+ycstuardo@users.noreply.github.com>
2022-12-26 05:56:22 +0000

    editing

--- index.js
+++ index.js
@@ -20,34 +20,3 @@ app.delete('/canciones/:id', (req, res) => {
   res.send('cancion se elimino con exito')
 })
 
-app.put('/canciones/:id', (req, res) => {
-  const { id } = req.params
-  const cancion = req.body
-  const canciones = JSON.parse(fs.readFileSync('canciones.json'))
-  const index = canciones.findIndex((song) => song.id == id)
-  canciones[index] = cancion
-  fs.writeFileSync('canciones.json', JSON.stringify(canciones))
-  res.send('cancion se modifico con exito')
-})
-
-app.post('/canciones', (req, res) => {
-  const cancion = req.body
-  const canciones = JSON.parse(fs.readFileSync('canciones.json'))
-  var existe = false
-  for (song of canciones) {
-    if((song.titulo == cancion.titulo) && (song.artista == cancion.artista) && (song.tono == cancion.tono)){
-        existe = true
-    }
-  }
-  if (existe) {
-    res.send('cancion ya se encuentra en la lista')
-  } else {
-    canciones.push(cancion)
-    fs.writeFileSync('canciones.json', JSON.stringify(canciones))
-    res.send('cancion se guardo con exito')
-  }
-})
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
