873a04578b6ae8d2ccff953fbe7e5b8efb5ea9fa
ycstuardo <104527883+ycstuardo@users.noreply.github.com>
2022-12-26 05:59:10 +0000

    lists of songs

--- index.js
+++ index.js
@@ -30,4 +30,25 @@ app.put('/canciones/:id', (req, res) => {
   res.send('cancion se modifico con exito')
 })
 
+app.post('/canciones', (req, res) => {
+  const cancion = req.body
+  const canciones = JSON.parse(fs.readFileSync('canciones.json'))
+  var existe = false
+  for (song of canciones) {
+    if((song.titulo == cancion.titulo) && (song.artista == cancion.artista) && (song.tono == cancion.tono)){
+        existe = true
+    }
+  }
+  if (existe) {
+    res.send('cancion ya se encuentra en la lista')
+  } else {
+    canciones.push(cancion)
+    fs.writeFileSync('canciones.json', JSON.stringify(canciones))
+    res.send('cancion se guardo con exito')
+  }
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
 
